ba9e7da61ea1a307d487751abee8abb69aa62a71
Darian Brown <browndarian011@gmail.com>
2023-01-22 16:14:39 -0500

    final changes

---
+++ script.js
@@ -0,0 +1,108 @@
+// building map w/ leaflet *****
+const myMap = {
+    coordinates: [],
+	businesses: [],
+	map: {},
+	markers: {},
+
+
+    
+// build leaflet map
+	buildMap() {
+        this.map = L.map('map', {
+            center: this.coordinates,
+            zoom: 11,
+		});
+		
+    // add streetmap tiles
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution:
+			'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            minZoom: '15',
+		}).addTo(this.map)
+		
+    // geolocation marker
+		const marker = L.marker(this.coordinates)
+		marker
+		.addTo(this.map)
+		.bindPopup('<p1><b>You are here</b><br></p1>')
+		.openPopup()
+        
+    // business marker
+       function addMarkers() {
+            for (var i = 0; i < this.business.length; i++){
+                this.markers = L.marker([
+                    this.businesses[i].lat,
+                    this.businesses[i].long,
+                ])
+                .bindPopup(`<p1>${this.businesses[i].name}</p1>`)
+                .addTo(this.map)
+            }
+        }
+        
+	},
+
+}
+    
+// get user location *************
+async function getCoords(){
+    const pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    });
+    return [pos.coords.latitude, pos.coords.longitude]
+    }
+    
+    
+    // submit button for business
+    document.getElementById('submit').addEventListener('click',async (event) => {
+        event.preventDefault()
+        let business = document.getElementById('business').value
+        let data = await getFoursquare(business)
+        myMap.businesses = processBusinesses(data)
+        myMap.addMarkers
+        console.log(business)
+    })
+    
+    
+    
+    
+// fourspuare API *********** API KEY: fsq3z9upl6O5Z4x9RMtx8StpW/AqO55Vuhv3ywRzThir5NI=
+async function getFoursquare(business) {
+	const options = {
+		method: 'GET',
+		headers: {
+		Accept: 'application/json',
+		Authorization: 'fsq3z9upl6O5Z4x9RMtx8StpW/AqO55Vuhv3ywRzThir5NI='
+		}
+	}
+	let limit = 5
+	let lat = myMap.coordinates[0]
+	let lon = myMap.coordinates[1]
+	let response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
+	let parsedData = JSON.parse(data)
+	let businesses = parsedData.results
+	return businesses
+}
+// foursquare array
+function processBusinesses(data) {
+	let businesses = data.map((element) => {
+		let location = {
+			name: element.name,
+			lat: element.geocodes.main.latitude,
+			long: element.geocodes.main.longitude
+		};
+		return location
+	})
+	return businesses
+}
+    
+
+
+
+ // window on load
+    window.onload = async () => {
+         const coords = await getCoords()
+            console.log(coords)
+            myMap.coordinates = coords
+            myMap.buildMap()
+        }
